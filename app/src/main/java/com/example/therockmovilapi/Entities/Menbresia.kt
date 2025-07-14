030ebeb1dde689a2f95aca4ec952375065facc7d
maicol2557M <maicolhurtado120@gmail.com>
2025-07-14 03:32:13 -0500

    ya esta la todas las apis

--- app/src/main/java/com/example/therockmovilapi/Entities/Menbresia.kt
+++ app/src/main/java/com/example/therockmovilapi/Entities/Menbresia.kt
@@ -1,9 +1,7 @@
 package com.example.therockmovilapi.Entities
 
-import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-@Entity (tableName = "menbresias")
 data class Menbresia(
     @PrimaryKey (autoGenerate = true) val id: Int = 0,
     val nombre: String,
